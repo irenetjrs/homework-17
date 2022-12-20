0d288143e16151f8e63593d0df7fecd8bfca2d49
irenetjrs <irkapidgorodetska@gmail.com>
2022-12-20 21:17:30 +0200

    alerts added

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 let text = 'You can ignore single and double quotes like this:  \\\' \\\'  \\\"  \\\"'
 console.log(text);
+alert('Please enter your data');
+let userName = prompt(
+   'Enter your name', 
+   'Name'
+);
+alert(`Your name is: ${userName}`);
+let userSurname = prompt(
+   'Enter your surname', 
+   'Surname'
+);
+alert(`Your full name is: ${userName} ${userSurname}`);
+let age = prompt(
+   'How old are you?', 
+   'Age'
+);
+alert(`Your full name is: ${userName} ${userSurname}, your age is: ${age}`);
+let admin = confirm(`Are you admin?`);
+alert(`Your full name is: ${userName} ${userSurname}, your age is: ${age} admin status: ${admin}`);
